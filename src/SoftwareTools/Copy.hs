7170f3d32935bafc04a19ca45628b47236f7da1f
Nathan Bloomfield <nbloomf@gmail.com>
2016-02-09 21:38:54 -0600

    Annotate copy

--- src/SoftwareTools/Copy.hs
+++ src/SoftwareTools/Copy.hs
@@ -1,3 +1,5 @@
+-- sth-copy: copy characters from stdin to stdout
+
 module Main where
 
 
@@ -9,9 +11,27 @@ import System.Exit
   (exitSuccess, exitFailure)
 
 
+{-
+This program copies characters from stdin to stdout until
+EOF is encountered, whereupon it exits. Any other error
+causes the program to fail.
+
+In an imperative language this pattern (read a character,
+write a character, repeat) would be written with a loop.
+In Haskell we instead use recursion.
+-}
+
 main :: IO ()
 main = catchIOError getChar handler >>= putChar >> main
 
+
+{-
+IO actions can throw IO errors, which (unlike other 
+error handling strategies in Haskell) can be silently,
+and thus accidentally, ignored. These errors are caught
+using catchIOError, which takes an action to be attempted
+and an error handler.
+-}
 
 handler :: IOError -> IO a
 handler err
